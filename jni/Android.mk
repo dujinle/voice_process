81be9a3a397c9d04d6e87e67315cc23457fe3915
dujinle <yingyongdujinle@163.com>
2017-12-12 07:20:49 +0000

    update for jni

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,42 +4,37 @@ LOCAL_PATH := $(call my-dir)
 $(info [${APP_ABI}] cur dir:${LOCAL_PATH})
 
 P_INCLUDE_PATH := ${LOCAL_PATH}/../include/
-P_LIB_PATH     := ${LOCAL_PATH}/../lib/android/
+P_INCLUDE_PATH += ${LOCAL_PATH}
 
 $(info [${APP_ABI}] NDK_ROOT dir ${NDK_ROOT})
 
 include $(CLEAR_VARS)
 $(info [${APP_ABI}] include dir:${P_INCLUDE_PATH})
-$(info [${APP_ABI}] libs dir:${P_LIB_PATH})
-LOCAL_MODULE    := landmarkreg
-
-LOCAL_C_INCLUDES:= $(P_INCLUDE_PATH) \
-	${LOCAL_PATH}/../src/
+LOCAL_MODULE    := libprocess
 
 # The order of these libraries is often important.
 
-LOCAL_LDFLAGS += \
-	"${P_LIB_PATH}libopencv_imgcodecs.a" \
-	"${P_LIB_PATH}libopencv_imgproc.a" \
-	"${P_LIB_PATH}libopencv_highgui.a" \
-	"${P_LIB_PATH}libopencv_core.a" \
-	"${P_LIB_PATH}libopencv_hal.a" \
-	"${P_LIB_PATH}liblibwebp.a" \
-	"${P_LIB_PATH}liblibpng.a" \
-	"${P_LIB_PATH}liblibjasper.a" \
-	"${P_LIB_PATH}liblibtiff.a" \
-	"${P_LIB_PATH}liblibjpeg.a" \
-	"${P_LIB_PATH}libIlmImf.a" \
-	"$(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi/thumb/libgnustl_static.a" \
-	"$(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi/thumb/libsupc++.a" \
+#LOCAL_LDFLAGS += \
+#	"$(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi/thumb/libgnustl_static.a" \
+#	"$(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi/thumb/libsupc++.a" \
 
-LOCAL_CPPFLAGS := -Wall -g -fpic \
-	-DDEBUG_LOG \
+LOCAL_C_INCLUDES = $(P_INCLUDE_PATH)
 
-LOCAL_SRC_FILES := $(wildcard ../src/*.cpp) \
-	jni.cpp
+LOCAL_CPPFLAGS := -Wall -Wint-to-pointer-cast -g -fpic \
+	-DANDROID_DEBUG_LOG \
+
+LOCAL_SRC_FILES := \
+	../src/core/cmfcc.c \
+	../src/core/compare.c \
+	../src/core/fft.c \
+	../src/core/mass_center.c \
+	../src/core/mel_bank.c \
+	../src/core/pre_process.c \
+	../src/core/tools.c \
+	../src/vad_snr/vad.c \
+	../src/process.c \
+	VprocessJNI.c
 
 LOCAL_LDLIBS    := -llog -lm -lz
-
 
 include $(BUILD_SHARED_LIBRARY)
---
+++ jni/jni.c
@@ -0,0 +1,110 @@
+#include <android/native_window_jni.h>
+#include "process.h"
+#include <jni.h>
+#include "log.h"
+
+//extern "C" {
+
+static JavaVM *g_vm = NULL;
+static jclass native_tm_class = NULL;
+static jobject lobj = NULL;
+
+jlong JNICALL nativeCreate(JNIEnv* env, jobject obj,jint size,jint fs,jint fsize,jint fmove){
+
+	lobj = env->NewGlobalRef(obj);
+	wav_info* winfo = init_winfo(size,fs,fsize,fmove);
+	if(winfo == NULL){
+#ifdef ANDROID_DEBUG_LOG
+		LOG("creat wav_info failed......");
+#endif
+		return -1;
+	}
+	jlong p_tm = reinterpret_cast<jlong>(winfo);
+#ifdef ANDROID_DEBUG_LOG
+	LOG("creat wav_info finish[%lld]",p_tm);
+#endif
+	return p_tm;
+}
+
+void JNICALL native_process(JNIEnv* env,jobject obj,jlong p_tm,jdoubleArray data){
+
+	jdouble* ddata = env->GetDoubleArrayElements(data,NULL);
+	if(NULL == ddata){ return; }
+
+	wav_info* native_tm = reinterpret_cast<wav_info*>(p_tm);
+	process_feat(native_tm,ddata);
+}
+
+void JNICALL nativeDestroy(JNIEnv* env, jobject obj, jlong p_tm) {
+
+	wav_info* native_tm = reinterpret_cast<wav_info*>(p_tm);
+	native_tm = NULL;
+#ifdef ANDROID_DEBUG_LOG
+	LOG("destroy thread_manager finish");
+#endif
+}
+
+jdoubleArray JNICALL native_compare(JNIEnv* env,jobject obj,jlong p1,jlong p2){
+	wav_info* w1 = reinterpret_cast<wav_info*>(p1);
+	wav_info* w2 = reinterpret_cast<wav_info*>(p2);
+	double* dist = compare_rec(w1,w2);
+	jdoubleArray iarr = env->NewDoubleArray(3);
+	env->SetDoubleArrayRegion(iarr,0,3,dist);
+	return iarr;
+}
+
+//}
+
+static JNINativeMethod methods[] = {
+	{ "create_obj", "()J", reinterpret_cast<void*>(&nativeCreate) },
+	{ "destroy", "(J)V", reinterpret_cast<void*>(&nativeDestroy)},
+	{ "process_feat", "(J[D)V", reinterpret_cast<void*>(&native_process)},
+	{ "compare", "(JJ)[D", reinterpret_cast<void*>(&native_compare)},
+};
+
+extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+
+	g_vm = vm;
+	JNIEnv* env = NULL;
+	if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+#ifdef ANDROID_DEBUG_LOG
+		LOG_ERROR("get env failed");
+#endif
+		return JNI_ERR;
+	}
+
+	native_tm_class = env->FindClass("com/cyx/landmark/LandMarkJNI");
+	if(native_tm_class == NULL){
+#ifdef ANDROID_DEBUG_LOG
+		LOG_ERROR("not found java class LandMarkJNI");
+#endif
+		return JNI_ERR;
+	}
+	native_tm_class = (jclass)env->NewGlobalRef(native_tm_class);
+
+	int len = sizeof(methods) / sizeof(methods[0]);
+	if (env->RegisterNatives(native_tm_class, methods,len) < 0) {
+#ifdef ANDROID_DEBUG_LOG
+		LOG_ERROR("register natives methods failed");
+#endif
+		return JNI_ERR;
+	}
+	return JNI_VERSION_1_6;
+}
+
+
+extern "C" JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved) {
+	JNIEnv* env = NULL;
+	if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+#ifdef ANDROID_DEBUG_LOG
+		LOG_ERROR("get env failed");
+#endif
+		return ;
+	}
+	if(native_tm_class != NULL){
+		env->DeleteLocalRef(native_tm_class);
+	}
+	if(lobj != NULL){
+		env->DeleteLocalRef(lobj);
+	}
+}
